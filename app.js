2cc20be2d6a61b00ef82506d34da12744a0833b9
wkava57 <wabutrune.p@hotmail.com>
2022-02-10 09:25:38 +0100

    feat: mon message

---
+++ app.js
@@ -0,0 +1,19 @@
+function conversionNombre(nb1,nb2){
+    nbConvert1 = parseInt(nb1);
+    nbConvert2 = parseInt(nb2);
+}
+
+function addition(nb1, nb2){
+    let calcul = nb1+nb2;
+    alert(calcul);
+}
+
+function soustraction(nb1, nb2){
+    let calcul = nb1-nb2;
+    alert(calcul);
+}
+
+function division(nb1, nb2){
+    let calcul = nb1/nb2;
+    alert(calcul);
+}
