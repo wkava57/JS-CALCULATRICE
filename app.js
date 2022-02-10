aebaf06ac2628e1f399f7687b9daaaa71054e299
wkava57 <wabutrune.p@hotmail.com>
2022-02-10 09:34:45 +0100

    fix: fonctions intégrées

--- app.js
+++ app.js
@@ -17,3 +17,19 @@ function division(nb1, nb2){
     let calcul = nb1/nb2;
     alert(calcul);
 }
+function checkNombre(nb1, nb2){
+    if(isNaN(nb1) || isNaN(nb2)){
+        alert("Veuillez saisir des nombres");
+        calculatrice();
+    }
+}
+
+function demandeNombre(){
+    nb1 = prompt("Veuillez saisir le premier nombre de l'opération");
+    nb2 = prompt("Veuillez saisir le deuxième nombre de l'opération");
+}
+
+function conversionNombre(nb1,nb2){
+    nbConvert1 = parseInt(nb1);
+    nbConvert2 = parseInt(nb2);
+}
